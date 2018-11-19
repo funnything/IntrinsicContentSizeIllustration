d500ff7fc18b56c9e71eeb4fd965d5c72af90cd3
funnything <coding.while.sleep@gmail.com>
2018-11-19 12:40:23 +0900

    Initial commit (gibo dump Windows macOS Linux Xcode Emacs Vim Swift Objective-C > .gitignore)

---
+++ IntrinsicContentSizeIllustration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntrinsicContentSizeIllustration
+//
+//  Created by Yosaku Toyama on 2018/11/19.
+//  Copyright © 2018年 DeNA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
